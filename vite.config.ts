944f96a86a57c24f74a443af26fb3603cb3670ee
yanivp01 <46996514+yanivp01@users.noreply.github.com>
2025-08-28 21:54:26 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
-  base: '/venturescholarstudio/', // <-- EXACTLY your repo name with leading & trailing slashes
+  base: '/vss-site/', // EXACT repo name, with leading + trailing slashes
 })
