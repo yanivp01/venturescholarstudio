e18062a03ef603ae62bb48bb4aefd74496adad40
yanivp01 <46996514+yanivp01@users.noreply.github.com>
2025-08-28 22:25:15 +0100

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-// vite.config.ts
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-export default defineConfig({
-  plugins: [react()],
-  base: '/vss-site/', // EXACT repo name, with leading + trailing slashes
-})
